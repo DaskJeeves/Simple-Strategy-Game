2b6b09bd25a132646cde22489dcd0910bb46ecbd
Dask <dask@transportasolutions.com>
2019-02-05 15:40:55 -0700

    First test example of RPS mobile application

---
+++ RPS/app/src/main/java/com/example/rock_paper_scissors/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.rock_paper_scissors
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the rock button */
+//    fun clickRockButton() {
+//        toast("Rock")
+//    }
+}
