4ae76ebcb9a75d3b67ca95dcd5e27efe60efd065
Dask <dask@transportasolutions.com>
2019-02-12 10:32:32 -0700

    New RPS attempt with cleaner UI

---
+++ rpsattempt/app/src/main/java/com/example/rps_attempt/Gameplay.kt
@@ -0,0 +1,12 @@
+package com.example.rps_attempt
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Gameplay : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gameplay)
+    }
+}
