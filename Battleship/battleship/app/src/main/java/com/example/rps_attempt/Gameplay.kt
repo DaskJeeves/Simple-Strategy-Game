e6d53ab0c9d718b43c57cdac8d411954656075d7
Dask <dask@transportasolutions.com>
2019-03-14 15:20:16 -0600

    Merged new grid into updated app code

--- Battleship/battleship/app/src/main/java/com/example/rps_attempt/Gameplay.kt
+++ Battleship/battleship/app/src/main/java/com/example/rps_attempt/Gameplay.kt
@@ -7,44 +7,90 @@ import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 
-class Gameplay : AppCompatActivity() {
 
-    val TAG = "GAMEPLAY"
+
+
+
+
+class Gameplay : AppCompatActivity(), View.OnClickListener {
+
+    var activePlayer = 1
+
+    val COLLECTION_KEY = "Game"
+
+    val DOCUMENT_KEY = "Room"
+
+    var MOVE_FIELD = "Move"
+
+    val firestoreChat by lazy{
+
+        FirebaseFirestore.getInstance().collection(COLLECTION_KEY).document(DOCUMENT_KEY)
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_gameplay)
+        val root = setContentView(R.layout.activity_gameplay)
+        realtimeUpdateListener()
 
-        val info = intent.extras
-        if (info != null) {
-            if(info.containsKey("tag")){
-                val game_name = findViewById<TextView>(R.id.game_name)
-                game_name.text = intent.getStringExtra("tag")
+
+        val tag = intent.getStringExtra("tag")
+        findViewById<TextView>(R.id.game_tag).text = tag
+
+    }
+
+
+
+    override fun onClick(v: View) {
+
+        Log.e("a","error occured")
+
+        val newMessage = mapOf(
+
+            MOVE_FIELD to v.getTag().toString())
+
+        firestoreChat.set(newMessage)
+
+            .addOnSuccessListener {
+
+                //Toast.makeText(this@Gameplay, "Message Sent", Toast.LENGTH_SHORT).show()
+                activePlayer = 0
             }
 
-        }else{
-            Log.i(TAG, "INTENT INFO: NULL")
+            .addOnFailureListener { e -> Log.e("ERROR", e.message) }
+
+    }
+
+
+    private fun realtimeUpdateListener() {
+
+        firestoreChat.addSnapshotListener { documentSnapshot, e ->
+
+            when {
+
+                e != null -> Log.e("ERROR", e.message)
+
+                documentSnapshot != null && documentSnapshot.exists() -> {
+
+                    with(documentSnapshot) {
+                        if (activePlayer == 0){
+                            var text = this!!.data?.get(MOVE_FIELD).toString() + " was selected"
+                            Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+                            activePlayer = 1
+                        }
+
+
+                    }
+
+                }
+
+            }
+
         }
-    }
 
-    fun choose_rock(view: View){
-        val text = "You Chose Rock"
-        val duration = Toast.LENGTH_SHORT
-
-        val toast = Toast.makeText(applicationContext, text, duration)
-        toast.show()
-    }
-
-    fun choose_paper(view: View){
-        val text = "You Chose Paper"
-        val duration = Toast.LENGTH_SHORT
-
-        val toast = Toast.makeText(applicationContext, text, duration)
-        toast.show()
-    }
-
-    fun finish_activity(view: View){
-        finish()
     }
 }
+
+
--- Battleship/battleship/app/src/main/java/com/example/rps_attempt/UserDashboard.kt
+++ Battleship/battleship/app/src/main/java/com/example/rps_attempt/UserDashboard.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
@@ -31,10 +33,10 @@ class UserDashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_dashboard)
+        realtimeUpdateListener()
 
         auth = FirebaseAuth.getInstance()
         val user = auth.currentUser
-        Log.e("USER", user!!.uid)
 
         val docRef = firestoreUser.document(user!!.uid)
         docRef.get()
@@ -51,11 +53,52 @@ class UserDashboard : AppCompatActivity() {
             .addOnFailureListener { exception ->
                 Log.e(TAG, "get failed with ", exception)
             }
+    }
+
+    /** Called when going to a gameplay page */
+    fun createNewGame(view: View) {
+        val uid = auth.currentUser!!.uid
+        val newMessage = mapOf(
+            "user1" to uid,
+            "user2" to "",
+            "status" to "active")
+
+        firestoreGame.document().set(newMessage)
+            .addOnSuccessListener {
+                Log.e("NEW GAME", "SUCCESS")
+            }
+            .addOnFailureListener { e -> Log.e("ERROR", e.message) }
+
+        goToGame(view)
+    }
+
+    /** Called when going to a gameplay page */
+    fun goToGame(view: View) {
+        val intent = Intent(this, Gameplay::class.java)
+        startActivity(intent)
+    }
+
+    fun loadActiveGames(){
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
 
         // Get all the active games where the logged in user is user1 or user2
         val active_games_ll = findViewById<LinearLayout>(R.id.active_games_linear_layout)
+
+        if ((active_games_ll).childCount > 0){
+            (active_games_ll).removeAllViews()
+        }
+
         val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
         lp.setMargins(10, 10, 10, 0)
+
+
+        val activeGameText = TextView(this)
+        activeGameText.text = "ACTIVE GAMES"
+        activeGameText.gravity = Gravity.CENTER
+        activeGameText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24.0F);
+        active_games_ll.addView(activeGameText, lp)
 
         val active_game_ref_1 = firestoreGame.whereEqualTo("user1", user!!.uid)
         active_game_ref_1.get()
@@ -79,6 +122,20 @@ class UserDashboard : AppCompatActivity() {
                         active_games_ll.addView(activeGameButton, lp)
                     }
                 }
+
+                //Add logout button to end
+                val lp2 = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                lp2.setMargins(10, 100, 10, 0)
+                val logoutText = TextView(this)
+                logoutText.text = "LOGOUT"
+                logoutText.gravity = Gravity.CENTER
+                logoutText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18.0F)
+                logoutText.setOnClickListener {
+                    auth = FirebaseAuth.getInstance()
+                    auth.signOut()
+                    finish()
+                }
+                active_games_ll.addView(logoutText, lp2)
             }
 
         // This part gets all the games where the logged in user is user2
@@ -103,27 +160,21 @@ class UserDashboard : AppCompatActivity() {
 //            }
     }
 
-    /** Called when going to a gameplay page */
-    fun createNewGame(view: View) {
-        val uid = auth.currentUser!!.uid
-        val newMessage = mapOf(
-            "user1" to uid,
-            "user2" to "",
-            "status" to "active")
+    private fun realtimeUpdateListener() {
 
-        firestoreGame.document().set(newMessage)
-            .addOnSuccessListener {
-                Log.e("NEW GAME", "SUCCESS")
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if (user != null) {
+            firestoreGame.whereEqualTo("user1", user.uid).addSnapshotListener { documentSnapshot, e ->
+                when {
+                    e != null -> Log.e("ERROR", e.message)
+                    documentSnapshot != null -> {
+                        loadActiveGames()
+                    }
+                }
             }
-            .addOnFailureListener { e -> Log.e("ERROR", e.message) }
-
-        goToGame(view)
-    }
-
-    /** Called when going to a gameplay page */
-    fun goToGame(view: View) {
-        val intent = Intent(this, Gameplay::class.java)
-        startActivity(intent)
+        }
     }
 
     /** Called when the user taps the Send button */
