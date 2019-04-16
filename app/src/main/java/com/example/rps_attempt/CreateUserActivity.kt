cf9cc6e8f94fee433962fea3a08288ac8051441d
aboyd9 <aboyd9@msudenver.edu>
2019-04-16 15:45:53 -0600

    Hello Guys

---
+++ app/src/main/java/com/example/rps_attempt/CreateUserActivity.kt
@@ -0,0 +1,128 @@
+package com.example.rps_attempt
+
+import android.content.Context
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.auth.UserProfileChangeRequest
+
+// TEST
+
+class CreateUserActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
+    private val firestoreUser by lazy {
+        FirebaseFirestore.getInstance().collection("Users")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_user)
+
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+         val currentUser = auth.currentUser
+        Log.e("USER LOGGED IN", "MANAGE DATA HERE")
+        if (currentUser != null) {
+            Log.e("USER LOGGED IN", currentUser.email)
+        }
+        //  updateUI(currentUser)
+    }
+
+    fun createUser(view: View) {
+        val usernameInput = findViewById<EditText>(R.id.new_user_username).text.toString()
+        val passwordInput = findViewById<EditText>(R.id.new_user_password).text.toString()
+        val emailInput = findViewById<EditText>(R.id.new_user_email).text.toString()
+        Log.e("CREATE", "USER")
+        createAccount(emailInput, passwordInput, usernameInput)
+    }
+
+    private fun createAccount(email: String, password: String, username: String) {
+        Log.e("createAccount", ":$email")
+
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                Log.e("TASK IS", task.toString())
+                if (task.isSuccessful) {
+                    Log.e("User created", ":success")
+                    val user = auth.currentUser
+                    // updateUI(user)
+
+                    if (user != null) {
+
+                        // Add display name
+//                        val profileUpdates = UserProfileChangeRequest.Builder()
+//                            .setDisplayName(username)
+//                            //.setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg"))
+//                            .build()
+//
+//                        user?.updateProfile(profileUpdates)
+//                            ?.addOnCompleteListener { task ->
+//                                if (task.isSuccessful) {
+//                                    Log.e("updated username to ", username)
+//                                } else {
+//                                    Log.e("failed update to ", username)
+//                                }
+//                            }
+
+                        // Send verification email
+//                        user?.sendEmailVerification()
+//                            ?.addOnCompleteListener { task ->
+//                                if (task.isSuccessful) {
+//                                    Log.e("Email verification.", "Sent")
+//                                } else {
+//                                    Log.e("failed verification to ", email)
+//                                }
+//                            }
+
+
+                        val uid = auth.currentUser!!.uid
+                        val newMessage = mapOf(
+                            "username" to username,
+                            "email" to email)
+                        firestoreUser.document(uid).set(newMessage)
+                            .addOnSuccessListener {
+                                Toast.makeText(this@CreateUserActivity, "User Created", Toast.LENGTH_SHORT).show()
+                            }
+                            .addOnFailureListener { e -> Log.e("ERROR", e.message) }
+
+
+                        auth.signInWithEmailAndPassword(email, username)
+
+
+                        val i = Intent()
+                        i.putExtra("user", username)
+                        setResult(RESULT_OK, i)
+                        finish()
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("createUserWithEmail", ":failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    // updateUI(null)
+                }
+            }
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/rps_attempt/UserDashboard.kt
@@ -0,0 +1,217 @@
+package com.example.rps_attempt
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.View
+import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
+
+// test
+
+class UserDashboard : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val firestoreUser by lazy {
+        FirebaseFirestore.getInstance().collection("Users")
+    }
+    private val firestoreGame by lazy {
+        FirebaseFirestore.getInstance().collection("Games")
+    }
+    private val TAG = "USERDASHBOARD"
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_dashboard)
+        realtimeUpdateListener()
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        val docRef = firestoreUser.document(user!!.uid)
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.e(TAG, "DocumentSnapshot data: " + document.data)
+                    // Capture the layout's TextView and set the string as its text
+                    val welcomeText = findViewById<TextView>(R.id.welcome_text)
+                    welcomeText.text = """     ${document.data!!["username"].toString()}"""
+                } else {
+                    Log.e(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "get failed with ", exception)
+            }
+
+        findViewById<ImageButton>(R.id.sync_active_games).setOnClickListener{
+            loadActiveGames()
+        }
+    }
+
+    /** Called when going to a gameplay page */
+    fun createNewGame(view: View) {
+        val uid = auth.currentUser!!.uid
+        val newMessage = mapOf(
+            "user1" to uid,
+            "user2" to "",
+            "status" to "active",
+            "created" to FieldValue.serverTimestamp()
+        )
+
+        val newGame = firestoreGame.document()
+
+        newGame.set(newMessage)
+            .addOnSuccessListener {
+                Log.e("NEW GAME", "SUCCESS")
+            }
+            .addOnFailureListener { e -> Log.e("ERROR", e.message) }
+
+        val intent = Intent(this, Gameplay::class.java)
+        intent.putExtra("tag", newGame.id)
+        startActivity(intent)
+    }
+
+    /** Called when going to a gameplay page */
+    fun goToGame(view: View) {
+        val intent = Intent(this, Gameplay::class.java)
+        startActivity(intent)
+    }
+
+    /** Called when going to a gameplay page */
+    fun goToChooseUser(view: View) {
+        val intent = Intent(this, ChooseUser::class.java)
+        startActivity(intent)
+    }
+
+    fun loadActiveGames(){
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        // Get all the active games where the logged in user is user1 or user2
+        val active_games_ll = findViewById<LinearLayout>(R.id.active_games_linear_layout)
+
+        if ((active_games_ll).childCount > 0){
+            (active_games_ll).removeAllViews()
+        }
+
+        val lp = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        lp.setMargins(10, 10, 10, 0)
+
+
+        val activeGameText = TextView(this)
+        activeGameText.text = "ACTIVE GAMES"
+        activeGameText.gravity = Gravity.CENTER
+        activeGameText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 24.0F);
+        active_games_ll.addView(activeGameText, lp)
+
+        val active_game_ref_1 = firestoreGame.whereEqualTo("user1", user!!.uid)
+        active_game_ref_1.get()
+            .addOnSuccessListener { document ->
+                for(doc in document){
+                    if (document != null) {
+                        val activeGameButton = Button(this)
+                        activeGameButton.text = doc.id
+                        activeGameButton.tag = doc.id
+                        activeGameButton.setBackgroundColor(
+                            resources.getColor(R.color.colorAccent)
+                        )
+                        activeGameButton.setTextColor(
+                            resources.getColor(R.color.white)
+                        )
+                        activeGameButton.setOnClickListener {
+                            val intent = Intent(this, Gameplay::class.java)
+                            intent.putExtra("tag", it.tag.toString())
+                            startActivity(intent)
+                        }
+                        active_games_ll.addView(activeGameButton, lp)
+                    }
+                }
+                // This part gets all the games where the logged in user is user2
+                // Should probably rewrite these two sections into their own function to reduce redundant code
+                // Also sort by most recent move
+
+                val active_game_ref_2 = firestoreGame.whereEqualTo("user2", user!!.uid)
+                active_game_ref_2.get()
+                    .addOnSuccessListener { document ->
+                        for(doc in document){
+                            if (document != null) {
+                                val activeGameButton = Button(this)
+                                activeGameButton.text = doc.id
+                                activeGameButton.tag = doc.id
+                                activeGameButton.setBackgroundColor(
+                                    resources.getColor(R.color.colorAccent)
+                                )
+                                activeGameButton.setTextColor(
+                                    resources.getColor(R.color.white)
+                                )
+                                activeGameButton.setOnClickListener {
+                                    val intent = Intent(this, Gameplay::class.java)
+                                    intent.putExtra("tag", it.tag.toString())
+                                    startActivity(intent)
+                                }
+                                active_games_ll.addView(activeGameButton, lp)
+                            }
+                        }
+
+                        //Add logout button to end
+                        val lp2 = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                        lp2.setMargins(10, 100, 10, 0)
+                        val logoutText = TextView(this)
+                        logoutText.text = "LOGOUT"
+                        logoutText.gravity = Gravity.CENTER
+                        logoutText.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18.0F)
+                        logoutText.setOnClickListener {
+                            auth = FirebaseAuth.getInstance()
+                            auth.signOut()
+                            finish()
+                        }
+                        active_games_ll.addView(logoutText, lp2)
+                    }
+
+            }
+
+
+    }
+
+    private fun realtimeUpdateListener() {
+
+        auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+
+        if (user != null) {
+            firestoreGame.whereEqualTo("user1", user.uid).addSnapshotListener { documentSnapshot, e ->
+                when {
+                    e != null -> Log.e("ERROR", e.message)
+                    documentSnapshot != null -> {
+                        loadActiveGames()
+                    }
+                }
+            }
+//            firestoreGame.whereEqualTo("user2", user.uid).addSnapshotListener { documentSnapshot, e ->
+//                when {
+//                    e != null -> Log.e("ERROR", e.message)
+//                    documentSnapshot != null -> {
+//                        loadActiveGames()
+//                    }
+//                }
+//            }
+        }
+    }
+
+    /** Called when the user taps the Send button */
+    fun signOutUser(view: View) {
+        auth = FirebaseAuth.getInstance()
+        auth.signOut()
+        finish()
+    }
+}
