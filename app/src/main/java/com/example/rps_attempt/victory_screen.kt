e02075aeef51e42ffa9c1661795af8e73d5c648c
aboyd9 <aboyd9@msudenver.edu>
2019-04-25 13:09:00 -0600

    activity for win condition (cont'd)

--- app/src/main/java/com/example/rps_attempt/victory_screen.kt
+++ app/src/main/java/com/example/rps_attempt/victory_screen.kt
@@ -1,22 +1,85 @@
 package com.example.rps_attempt
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.FirebaseFirestore
 
 import kotlinx.android.synthetic.main.activity_victory_screen.*
 
 class victory_screen : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val firestoreUser by lazy {
+        FirebaseFirestore.getInstance().collection("Users")
+    }
+    private val firestoreGame by lazy {
+        FirebaseFirestore.getInstance().collection("Games")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_victory_screen)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+
+        // GET THE USER THAT WON AND DISPLAY IT!
+
+        /*var hitCount = 0
+        for(doc in opponentShipsSnapshot) {
+            val opponentShipid = resources.getIdentifier(doc.data!!["position"].toString().toLowerCase(), "id", packageName)
+            for(doc in userShipsSnapshot) {
+                val id = resources.getIdentifier(doc.data!!["position"].toString().toLowerCase(), "id", packageName)
+                if (opponentShipid==id){
+                    hitCount++
+                }
+            }
         }
+        if (hitCount >= 6)
+        {
+
+            val intent = Intent(this, victory_screen::class.java).apply {
+            }
+            startActivity(intent)
+        }
+        */
+
+
+
+    }
+    fun gotoMenu(v: View) {
+        val intent = Intent(this, UserDashboard::class.java)
+        startActivity(intent)
+
+
     }
 
-}
+    fun createNewGame(view: View) {
+            val uid = auth.currentUser!!.uid
+            val newMessage = mapOf(
+                "user1" to uid,
+                "user2" to "",
+                "status" to "active",
+                "created" to FieldValue.serverTimestamp()
+            )
+
+            val newGame = firestoreGame.document()
+
+            newGame.set(newMessage)
+                .addOnSuccessListener {
+                    Log.e("NEW GAME", "SUCCESS")
+                }
+                .addOnFailureListener { e -> Log.e("ERROR", e.message) }
+
+            val intent = Intent(this, Gameplay::class.java)
+            intent.putExtra("tag", newGame.id)
+            startActivity(intent)
+        }
+
+    }
+
